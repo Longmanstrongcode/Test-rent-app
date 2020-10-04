803377a807e520cfd3e007170183ea4fd0fc8abe
Longmanstrongcode <kiyashenko.front@gmail.com>
2020-10-04 18:33:00 +0300

    pr

---
+++ client/src/components/RentedList.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react"
+
+class RentedList extends Component{
+    render(){
+        return(
+               <div>
+                   <h2>You Rent:($totalprice) </h2>
+
+               </div>
+        )
+    }
+}
+
+export default RentedList;
